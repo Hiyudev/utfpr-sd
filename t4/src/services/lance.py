76d0cd763c3797255eb877b4d914bc81ac3f6dc0
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2025-10-28 14:16:03 -0300

    feat: implement item 5

--- t4/src/services/lance.py
+++ t4/src/services/lance.py
@@ -6,10 +6,6 @@ import datetime
 import sys
 import os
 
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization, hashes
-
 # Adiciona o diretório raiz do projeto ao sys.path para importar 'common'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -51,77 +47,42 @@ def main():
         if method.routing_key == "lance_realizado":
             lance = deserialize_dict(body)
 
-            # necessario converter a assinatura e valor para bytes pra permitir o processo de verificacao
-            b_signature = base64.b64decode(lance["signature"])
-            # b_value = lance['value'].encode('utf-8')
-            b_message = json.dumps(
-                {
-                    "user_id": lance["user_id"],
-                    "leilao_id": lance["leilao_id"],
-                    "value": lance["value"],
-                }
-            ).encode("utf-8")
-
-            # esses blocos try catch estavam mais para encontrar os erros que estavam aparecendo, se achar feio pode retirar
-
-            # Requisito 4.1 - Possui as chaves públicas de todos os clientes.
-            try:
-                with open(f"./keys/{lance['user_id']}.pem", "rb") as f:
-                    public_key_data = f.read()
-            except:
-                print("[MS-Lance] abertura de arquivo falhou!")
-
-            try:
-                public_key = load_pem_public_key(public_key_data)
-            except:
-                print("[MS-Lance] carregamento da chave falhou!")
-
-            try:
-                public_key.verify(
-                    b_signature,
-                    b_message,
-                    padding.PSS(
-                        mgf=padding.MGF1(hashes.SHA256()),
-                        salt_length=padding.PSS.MAX_LENGTH,
-                    ),
-                    hashes.SHA256(),
-                )
-            except Exception as e:
-                print("[MS-Lance] Exception: ", type(e))
-            else:
-                print("[MS-Lance] Assinatura valida!")
-
-                # checa se id do leilao existe em leiloes
-                if any(lance["leilao_id"] in d["id"] for d in leiloes):
-                    print("[MS-Lance] leilao existe!")
-                    # checa se eh maior lance
-                    lance_vencedor = [
-                        d.get("highest_bid")
+            # checa se id do leilao existe em leiloes
+            if any(lance["leilao_id"] in d["id"] for d in leiloes):
+                print("[MS-Lance] leilao existe!")
+                # checa se eh maior lance
+                lance_vencedor = [
+                    d.get("highest_bid")
+                    for d in leiloes
+                    if lance["leilao_id"] in d["id"]
+                ]
+                if int(lance["value"]) > int(lance_vencedor[0]):
+                    # Requisito 4.4 - Se o lance for válido, o MS Lance publica o evento na fila lance_validado.
+                    [
+                        d.update(highest_bid=lance["value"])
                         for d in leiloes
                         if lance["leilao_id"] in d["id"]
                     ]
-                    if int(lance["value"]) > int(lance_vencedor[0]):
-                        # Requisito 4.4 - Se o lance for válido, o MS Lance publica o evento na fila lance_validado.
-                        [
-                            d.update(highest_bid=lance["value"])
-                            for d in leiloes
-                            if lance["leilao_id"] in d["id"]
-                        ]
-                        [
-                            d.update(winner=lance["user_id"])
-                            for d in leiloes
-                            if lance["leilao_id"] in d["id"]
-                        ]
-                        channel.basic_publish(
-                            exchange=EXCHANGE_NAME,
-                            body=body,
-                            routing_key="lance_validado",
-                        )
-                        print("[MS-Lance] lance validado!")
-                    else:
-                        print("[MS-Lance] lance nao eh maior que atual!")
+                    [
+                        d.update(winner=lance["user_id"])
+                        for d in leiloes
+                        if lance["leilao_id"] in d["id"]
+                    ]
+                    channel.basic_publish(
+                        exchange=EXCHANGE_NAME,
+                        body=body,
+                        routing_key="lance_validado",
+                    )
+                    print("[MS-Lance] Lance validado!")
                 else:
-                    print("[MS-Lance] leilao nao existe!")
+                    channel.basic_publish(
+                        exchange=EXCHANGE_NAME,
+                        body=body,
+                        routing_key="lance_invalidado"
+                    )
+                    print("[MS-Lance] Lance invalidado!")
+            else:
+                print("[MS-Lance] leilao nao existe!")
 
         if method.routing_key == "leilao_iniciado":
             # Somente aceitará o lance se: ID do leilão existir e se o leilão estiver ativo;
